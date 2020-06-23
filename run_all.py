af37e5f42d64e737dea935182880d1b0b405b75d
zhaolili-sun <1451770403@qq.com>
2020-06-23 13:59:35 +0800

    run

--- run_all.py
+++ run_all.py
@@ -5,10 +5,10 @@ from run import *
 #suite 可以各种组合定义
 # suite = unittest.defaultTestLoader.discover("E:\Python\python_unittest\Test\CMS")#用例地址
 
-# list1 = makesuite_by_testlist('E:\Python\python_unittest\Test\CMS\\testlist.txt')
-# print(list1)
-# run(list1)
-# run_all()
+list1 = makesuite_by_testlist('E:\Python\python_unittest\Test\CMS\\testlist.txt')
+print(list1)
+run(list1)
+run_all()
 run(collect())
 
 
